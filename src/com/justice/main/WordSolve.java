63416a3324610f323513868ad5acb03b8cfda32d
Justice-Owens <justice.h.owens@gmail.com>
2023-09-25 14:55:20 -0400

    removed a line

---
+++ src/com/justice/main/WordSolve.java
@@ -0,0 +1,11 @@
+package com.justice.main;
+
+import java.util.HashMap;
+
+public class WordSolve {
+    private HashMap<String, Integer> wordMap;
+
+    public WordSolve(HashMap<String, Integer> wordMap) {
+        this.wordMap = wordMap;
+    }
+}
